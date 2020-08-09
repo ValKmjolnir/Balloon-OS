e7fa271747acc35ca30a7988eea7b5a1a1bf137d
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2020-08-09 02:19:27 -0700

    update

--- bootsect.s
+++ bootsect.s
@@ -16,8 +16,13 @@ bssbeg:
 .equ BOOTSEG, 0x07c0
 # begin from 0x07c0:0x0000->0x07c00
 .equ INITSEG, 0x9000
-# jump to 0x9000:0x0000->0x90000
+# jump to 0x9000:0x0000->0x90000 then bootsect 0x90000~0x901ff
+.equ SETUPSEG,0x9020
+# beginning address of setup.s
 .equ SYSSEG, 0x1000
+# beginning address of system
+.equ SYSEND, 0x4220
+# system ends at 0x42000,0x42000-0x10000=0x32000=200kb
 
 
 ljmp $BOOTSEG,$bootstart
@@ -43,46 +48,101 @@ stackset:
 	mov $0xff00,%sp # set sp=0xff00
 
 load_setup:
-	mov $0x0000,%dx   # DH(head) DL(drive)
-	mov $0x0002,%cx   # CH(track) CL(sector)
-	mov $0x0204,%ax   # AH(0x02 read) AL(4 sectors)
-	mov $0x0200,%bx   # es is alreay 0x9000,file will be readed to 0x9000:0x0200->0x90200
-	int $0x13         # BIOS drive service
-	jnc ok_load_setup # if read failed carry flag will be 1
+	mov $0x0000,%dx    # DH(0 head) DL(0 drive)
+	mov $0x0002,%cx    # CH(0 track) CL(from 2 sector)
+	mov $0x0204,%ax    # AH(0x02 read) AL(4 sectors)
+	mov $0x0200,%bx    # es is alreay 0x9000,file will be readed to 0x9000:0x0200->0x90200
+	int $0x13          # BIOS drive service
+	jnc get_sector_num # if read failed carry flag will be 1
+
 	mov $0x0000,%dx
 	mov $0x0000,%ax   # ah=0x00 means use BIOS reset floppy/disk
 	int $0x13         # reset
 	jmp load_setup
 
-ok_load_setup:
+get_sector_num:
 	mov $0x0000,%dx # dl=0x00 means it is floppy,if it is disk,dl[7]=1
 	mov $0x0800,%ax # ah=0x08 means get drive parameters
-	int $0x13        # get it,after this int,es will be set to an address points to parameter list
-	
+	int $0x13       # get it,after this int,es will be set to an address points to parameter list
 	mov %cx,%cs:sectors+0 # store number of sectors of each track in cs:sectors
-	
-	mov $INITSEG,%ax # reset es=INITSEG
+
+before_load_system:
+	xor %bx,%bx     # set bx=0x0000
+	mov $0x0000,%dx # DH(0 head) DL(0 drive[floppy])
+	mov $0x0005,%cx # CH(0 track)CL(5 sector)
+	mov $SYSSEG,%ax
+	mov %ax,%es     # set es=0x1000
+
+load_system:
+	mov %cs:sectors+0,%ax # load sector_number to ax
+	cmpb %al,%cl     # if CL(sector)==sector_number means need to update DH(head) and CH(track)
+	jne load_process # if not equal,continue loading
+	cmpb $0x01,%dh   # if DH==1,CH+1
+	jne set_new_dx_cx # if not equal,skip adding 1 to CH
+	addb $0x01,%ch    # add 1 to CH to change the track
+
+set_new_dx_cx:
+	xorb $0x01,%dh    # change DH,DH=0x00 then DH is set to 0x01,else DH is set to 0x00
+	xorb %cl,%cl
+
+load_process:
+	addb $0x01,%cl
+	mov $0x0201,%ax
+	int $0x13
+	jc failed_load_system
+
+	mov %es,%ax
+	addw $0x20,%ax # add 0x20 to es,then es:bx is added 0x200=512 bytes
 	mov %ax,%es
 
-printinfo:
+	cmpw $SYSEND,%ax # if es==SYSEND,end loading and print info string
+	je ok_load_system
+	jmp load_system # continue loading
+
+ok_load_system:
 	mov $0x03,%ah   # read cursor position
 	xor %bh,%bh     # set page 0
 	int $0x10
 
-	mov $startstr,%bp # set string address
-	mov $0x1301,%ax   # write string,move cursor
-	mov $0x0007,%bx   # page 0,black background/white characters
-	mov $30,%cx       # length of string
-	int $0x10         # 0x10 video service
-	# print start string
+	mov $INITSEG,%ax
+	mov %ax,%es     # es:bp points to the string
 
-loop:
+	mov $sysmsg,%bp # set string address
+	mov $0x1301,%ax # write string,move cursor
+	mov $0x0007,%bx # page 0,black background/white characters
+	mov $28,%cx     # length of string
+	int $0x10       # 0x10 video service
+	ljmp $SETUPSEG,$0
+
+failed_load_system:
+	mov $0x0000,%dx
+	mov $0x0000,%ax   # ah=0x00 means use BIOS reset floppy/disk
+	int $0x13         # reset
+
+	mov $0x03,%ah   # read cursor position
+	xor %bh,%bh     # set page 0
+	int $0x10
+
+	mov $INITSEG,%ax
+	mov %ax,%es      # es:bp points to the string
+
+	mov $failmsg,%bp # set string address
+	mov $0x1301,%ax  # write string,move cursor
+	mov $0x0007,%bx  # page 0,black background/white characters
+	mov $24,%cx      # length of string
+	int $0x10        # 0x10 video service
+
+die:
 	hlt
-	jmp loop
+	jmp die # if failed to load the system,enter this infinite loop
 
-startstr:
+sysmsg:
 	.ascii "Starting Balloon System..."
-	.byte 13,10,13,10
+	.byte 13,10
+
+failmsg:
+	.ascii "Loading System failed."
+	.byte 13,10
 
 sectors:
 	.word 0
--- setup.s
+++ setup.s
@@ -1,2 +1,41 @@
+# balloon system setup.s
+# copyright @ValKmjolnir
+# this is only a demo now,i'll change this file later
+
+.code16
+.global setup
+.global databeg,dataend,bssbeg,bssend,textbeg,textend
+
+.text
+textbeg:
+.data
+databeg:
+.bss
+bssbeg:
+.text
+
+.equ SETUPSEG,0x9020
+# beginning address of setup.s
+
 setup:
+	hlt
+	mov $SETUPSEG,%ax
+	mov %ax,%es      # set es=SETUPSEG to use es:bp points to info string
+
+	mov $info,%bp    # set string address
+	mov $0x1301,%ax  # write string,move cursor
+	mov $0x0007,%bx  # page 0,black background/white characters
+	mov $31,%cx      # length of string
+	int $0x10        # 0x10 video service
 	jmp setup
+
+info:
+	.ascii "You are now in setup process."
+	.byte 13,10
+
+.text
+textend:
+.data
+dataend:
+.bss
+bssend:
